33c143b3a795166e70241bc800d91a215f9bad0f
Leah <LeahKahwadjian@gmail.com>
2020-06-25 18:31:11 -0400

    Updates

---
+++ precache-manifest.f1b53d578b837cb000d716e1c4d71ca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c031575c6a429f69de78e8c614b1851",
+    "url": "/simpsons-keyboard-react/index.html"
+  },
+  {
+    "revision": "76089905b014bbc6aa36",
+    "url": "/simpsons-keyboard-react/static/css/main.2659d9b0.chunk.css"
+  },
+  {
+    "revision": "bcfd611288b04f18dddd",
+    "url": "/simpsons-keyboard-react/static/js/2.c2069381.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simpsons-keyboard-react/static/js/2.c2069381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76089905b014bbc6aa36",
+    "url": "/simpsons-keyboard-react/static/js/main.303fa2e0.chunk.js"
+  },
+  {
+    "revision": "4a83fb19cb538e3dd842",
+    "url": "/simpsons-keyboard-react/static/js/runtime-main.40d340e1.js"
+  }
+]);
