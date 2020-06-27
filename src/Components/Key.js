2b4635a8c3c5495ab74693b423e29cdc01c49808
Leah <LeahKahwadjian@gmail.com>
2020-06-26 23:57:30 -0400

    added files for sounds & images, started logic for handler

--- src/Components/Key.js
+++ src/Components/Key.js
@@ -3,29 +3,49 @@ import styled from "styled-components";
 
 class Key extends Component {
   render() {
-    console.log("key ", this.props);
+    console.log("audio ", this.props);
+    const handlerFx = (e) => {
+      const audioFile = e.currentTarget.querySelectorAll("img")[0].alt;
+      console.log(`audio is ${audioFile}`);
+    };
+
     return (
-      <StyledKey>
-        <p>{this.props.name}</p>
+      <StyledKey
+        className="thestyledkey"
+        data-key={this.props.dataKey}
+        onClick={handlerFx}
+      >
+        <p className="character-name">{this.props.name}</p>
         <img
           className="character-image"
           src={`images/${this.props.image}`}
           alt={this.props.name}
         />
-        <p>{this.props.kbd}</p>
+        <kbd className="character-keycode">{this.props.kbd}</kbd>
       </StyledKey>
     );
   }
 }
 
 const StyledKey = styled.div`
-  min-width: 110px;
-  min-height: 110px;
-  border: 0.4rem solid #ace;
-  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  border: 0.2rem solid #ace;
+  padding: 5px 0 10px;
   background: rgba(190, 70, 220, 0.3);
+  .character-name {
+    min-height: 60px;
+    text-transform: uppercase;
+    color: #ffdf00;
+    font-size: 0.7rem;
+  }
   .character-image {
     max-height: 50px;
+  }
+  .character-keycode {
+    color: #eee;
   }
 `;
 
--- src/Components/Keyboard.js
+++ src/Components/Keyboard.js
@@ -4,20 +4,75 @@ import Key from "./Key";
 
 class Keyboard extends Component {
   render() {
-    console.log("Keyboard: ", this.props);
     return (
-      <StyledKeyboard className="keyboard">
-        {this.props.keys.map((key) => {
-          return <Key name={key.name} image={key.image} kbd={key.kbd} />;
-        })}
+      <StyledKeyboard>
+        <div className="keyboard-row ">
+          {this.props.keys.map((key, i) => (
+            <>
+              {typeof key.kbd === "number" ? (
+                <Key
+                  name={key.name}
+                  image={key.image}
+                  kbd={key.kbd}
+                  dataKey={key.dataKey}
+                ></Key>
+              ) : null}
+            </>
+          ))}
+        </div>
+        <div className="keyboard-row ">
+          {this.props.keys.map((key, i) => (
+            <>
+              {i >= 10 && i < 20 ? (
+                <Key
+                  name={key.name}
+                  image={key.image}
+                  kbd={key.kbd}
+                  dataKey={key.dataKey}
+                ></Key>
+              ) : null}
+            </>
+          ))}
+        </div>
+        <div className="keyboard-row ">
+          {this.props.keys.map((key, i) => (
+            <>
+              {i >= 20 && i < 29 ? (
+                <Key
+                  name={key.name}
+                  image={key.image}
+                  kbd={key.kbd}
+                  dataKey={key.dataKey}
+                ></Key>
+              ) : null}
+            </>
+          ))}
+        </div>
+        <div className="keyboard-row">
+          {this.props.keys.map((key, i) => (
+            <>
+              {i >= 29 && i < 37 ? (
+                <Key
+                  name={key.name}
+                  image={key.image}
+                  kbd={key.kbd}
+                  dataKey={key.dataKey}
+                ></Key>
+              ) : null}
+            </>
+          ))}
+        </div>
       </StyledKeyboard>
     );
   }
 }
 
 const StyledKeyboard = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+  .keyboard-row {
+    display: flex;
+    justify-content: space-around;
+    width: 90vw;
+  }
 `;
 
 export default Keyboard;
